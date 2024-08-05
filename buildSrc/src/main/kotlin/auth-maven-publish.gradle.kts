7ccd16cc29357363a839053c9fd608db3e5ba8f7
llh4github <lilinhong_coding@foxmail.com>
2024-08-05 14:42:14 +0800

    test: 测试发布脚本

--- buildSrc/src/main/kotlin/auth-maven-publish.gradle.kts
+++ buildSrc/src/main/kotlin/auth-maven-publish.gradle.kts
@@ -12,6 +12,17 @@ val jarJavadoc by tasks.registering(Jar::class) {
     archiveClassifier.set("javadoc")
 }
 publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/llh4github/simple-auth")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME_GITHUB")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("PAT_GITHUB")
+            }
+        }
+
+    }
     publications {
 
         create<MavenPublication>("dist") {
@@ -24,36 +35,5 @@ publishing {
             version = project.version.toString()
         }
 
-        repositories {
-            maven {
-                name = "central"
-                url = uri("https://s01.oss.sonatype.org/content/repositories/releases/")
-                credentials {
-                    username = System.getenv("OSSRH_USERNAME")
-                    password = System.getenv("OSSRH_PASSWORD")
-                }
-            }
-            maven {
-                name = "snapshot"
-                url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
-                credentials {
-                    username = System.getenv("OSSRH_USERNAME")
-                    password = System.getenv("OSSRH_PASSWORD")
-                }
-            }
-        }
-
-
     }
-}
-signing {
-    val keyId = System.getenv("GPG_KEY_ID")
-    val secretKey = System.getenv("GPG_SECRET_KEY")
-    val password = System.getenv("GPG_PASSWORD")
-//    setRequired {
-//        !project.version.toString().endsWith("SNAPSHOT")
-//    }
-    isRequired = false
-    useInMemoryPgpKeys(keyId, secretKey, password)
-    sign(publishing.publications["dist"])
 }
