21ac7e04eea0812b7fd5f5bdd125e8cbf612bdc0
llh4github <61351434+llh4github@users.noreply.github.com>
2024-08-05 22:22:12 +0800

    build: 上传maven中央仓库配置 (#4)
    * test: 测试发布脚本
    
    * build: 测试发布脚本
    
    * build: action name
    
    * build: load credentials
    
    * build: load credentials
    
    * build: script test
    
    * build: script test
    
    * build: 删除GPG配置

--- buildSrc/src/main/kotlin/auth-maven-publish.gradle.kts
+++ buildSrc/src/main/kotlin/auth-maven-publish.gradle.kts
@@ -11,7 +11,27 @@ val jarSources by tasks.registering(Jar::class) {
 val jarJavadoc by tasks.registering(Jar::class) {
     archiveClassifier.set("javadoc")
 }
+
 publishing {
+    repositories {
+        maven {
+            name = "central"
+            url = uri("https://s01.oss.sonatype.org/content/repositories/releases/")
+            credentials {
+                username = project.findProperty("OSSRH_USERNAME") as String? ?: System.getenv("OSSRH_USERNAME")
+                password = project.findProperty("OSSRH_PASSWORD") as String? ?: System.getenv("OSSRH_PASSWORD")
+            }
+        }
+
+        maven {
+            name = "snapshot"
+            url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
+            credentials {
+                username = project.findProperty("OSSRH_USERNAME") as String? ?: System.getenv("OSSRH_USERNAME")
+                password = project.findProperty("OSSRH_PASSWORD") as String? ?: System.getenv("OSSRH_PASSWORD")
+            }
+        }
+    }
     publications {
 
         create<MavenPublication>("dist") {
@@ -24,36 +44,5 @@ publishing {
             version = project.version.toString()
         }
 
-        repositories {
-            maven {
-                name = "central"
-                url = uri("https://s01.oss.sonatype.org/content/repositories/releases/")
-                credentials {
-                    username = System.getenv("OSSRH_USERNAME")
-                    password = System.getenv("OSSRH_PASSWORD")
-                }
-            }
-            maven {
-                name = "snapshot"
-                url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
-                credentials {
-                    username = System.getenv("OSSRH_USERNAME")
-                    password = System.getenv("OSSRH_PASSWORD")
-                }
-            }
-        }
-
-
     }
-}
-signing {
-    val keyId = System.getenv("GPG_KEY_ID")
-    val secretKey = System.getenv("GPG_SECRET_KEY")
-    val password = System.getenv("GPG_PASSWORD")
-//    setRequired {
-//        !project.version.toString().endsWith("SNAPSHOT")
-//    }
-    isRequired = false
-    useInMemoryPgpKeys(keyId, secretKey, password)
-    sign(publishing.publications["dist"])
 }
